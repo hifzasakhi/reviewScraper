817756064defc630d9e584e82fc8775391e76253
Hifza Sakhi <hifzasakhi@gmail.com>
2017-08-29 01:19:26 -0400

    Pulls out the string from the a tag in the first review

--- scraper.py
+++ scraper.py
@@ -16,7 +16,13 @@ def getNumWebPages(page_url):
 	print "got the req"
 	soup = BeautifulSoup(req.content, "html.parser")
 	print "got the soup obj"
-	print(soup.prettify())
+	#print(soup.prettify())
+
+	#review comments are under a tag, with class='comment'
+	reviewComments = soup.find('a', class_="comment")
+	for r in reviewComments: 
+		print(reviewComments.string) 
+	#print(soup.find_all('a', class_="comment"))
 	return soup
 
 
