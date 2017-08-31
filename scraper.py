287b2146a7bdd5b23c60a043cd72b333b1ce9e5b
Hifza Sakhi <hifzasakhi@gmail.com>
2017-08-30 23:58:15 -0400

    added a function to determine max number of pages with reviews and to scrape all those pages for reviews

--- scraper.py
+++ scraper.py
@@ -5,42 +5,49 @@ from textblob import TextBlob
 def getURLFromUser():
 	#Example url: http://www.ioffer.com/ratings/bags789/new-handbags-bags-shoulder-bag-627601317
 	url = raw_input("Please enter the review url for seller of a product you need: ")
-	print "you entered the following url: ", url
+	print "you entered: ", url
 	return url
 
-#page_url = ioffer.com url
-def getCommentAndSentiment(page_url):
+def getNumPagesToScrape(page_url):
 
-	#Going to scrape 3 latest pages of reviews 
+	#use a longNum value to determine max number of pages containing reviews
+	longNum = 1000000000000
+	url = page_url + "?page=" + str(longNum) + "&sort=default"
+	print "modified page url to get page_nums is :", url
 
-	for i in range (1, 7): 
-		if i == 1: 
-			url = page_url 
-		else:
-			#http://www.ioffer.com/ratings/bags789/new-handbags-bags-shoulder-bag-627601317?page=2&sort=default
-			url = page_url + "?page=" + str(i) + "&sort=default" 
-			print "modified page url is :", url	
 	req = requests.get(url)
-
 	soup = BeautifulSoup(req.content, "html.parser")
+	pagination = soup.find("div", class_="pagination")
+	anchors = pagination.find_all("a")
+
+	#the last anchor contains the num_pages
+	num_pages = anchors[len(anchors)-1].text
+	print(num_pages)
+
+	#conver Unicode to int before returning
+	return int(num_pages)
 	
+def getCommentAndSentiment(page_url, num_pages):
+
+	#Scrapes reviews on all available pages 
+	for i in range (1, num_pages + 1): 
+		url = page_url + "?page=" + str(i) + "&sort=default" 
+		
+	
+	req = requests.get(url)
+	soup = BeautifulSoup(req.content, "html.parser")
 	reviewComments = soup.find_all("a", class_="comment")
 
-
-	for r in reviewComments: 
-		blob = TextBlob(r.text)
-		print "pageNum: ", i, " ", r.text , "==> polarity:" , str(blob.sentiment.polarity), " ; " \
+	for revComment in reviewComments: 
+		blob = TextBlob(revComment.text)
+		print "pageNum: ", i, " ", revComment.text , \
+		"==> polarity:" , str(blob.sentiment.polarity), " ; " \
 		"subjectivity:", str(blob.sentiment.subjectivity)
-		#using TextBlob library to do Sentiment Analysis
-		
-		#print(blob.sentiment.polarity)
-		
-	#return soup
-
 
 def main():
 	page_url = getURLFromUser()
-	getCommentAndSentiment(page_url);
+	num_pages = getNumPagesToScrape(page_url)
+	getCommentAndSentiment(page_url, num_pages);
 
 if __name__ == "__main__":
   main()
